1b43e5c3acf719698ff486b64717e85fdc2af554
Vishwajeet Pratap Singh <101934336+vishwajeet0001@users.noreply.github.com>
2025-05-03 11:39:49 +0530

    Update bfsdfs.cpp

--- LP5/HPC/bfsdfs.cpp
+++ LP5/HPC/bfsdfs.cpp
@@ -1,73 +1,217 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <chrono>
+#include <random>
 #include <omp.h>
 
 using namespace std;
 
-#define MAX_NODES 100
+class Graph
+{
+public:
+    int vertices;
+    vector<vector<int>> graph;
+    vector<bool> visited;
 
-vector<vector<int>> adj(MAX_NODES, vector<int>(MAX_NODES));  // Graph representation
-vector<bool> visited(MAX_NODES, false);  // Visited nodes
+    Graph(int v, int e) : vertices(v)
+    {
+        graph.resize(vertices);
+        generate_random_graph(e);
+    }
 
-// Parallel BFS function
-void parallel_bfs(int start_node, int num_nodes) {
-    vector<int> queue;
-    queue.push_back(start_node);
-    visited[start_node] = true;
+    void generate_random_graph(int edges)
+    {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> dist(0, vertices - 1);
 
-    int front = 0;
-    while (front < queue.size()) {
-        int level_size = queue.size() - front;
+        for (int i = 0; i < edges; i++)
+        {
+            int u = dist(gen);
+            int v = dist(gen);
+            if (u != v)
+            {
+                graph[u].push_back(v);
+                graph[v].push_back(u);
+            }
+        }
+    }
 
-        #pragma omp parallel for
-        for (int i = 0; i < level_size; i++) {
-            int node = queue[front + i];
-            cout << "BFS Visited: " << node << endl;
+    void initialize_visited()
+    {
+        visited.assign(vertices, false);
+    }
 
-            for (int j = 0; j < num_nodes; j++) {
-                if (adj[node][j] && !visited[j]) {
-                    visited[j] = true;
-                    queue.push_back(j);
+    void parallel_dfs(int start)
+    {
+        visited[start] = true;
+#pragma omp parallel
+        {
+            stack<int> local_stack;
+            local_stack.push(start);
+
+            while (!local_stack.empty())
+            {
+                int current;
+#pragma omp critical
+                {
+                    if (!local_stack.empty())
+                    {
+                        current = local_stack.top();
+                        local_stack.pop();
+                    }
+                    else
+                    {
+                        current = -1;
+                    }
+                }
+
+                if (current == -1)
+                    continue;
+
+                for (int neighbor : graph[current])
+                {
+                    if (!visited[neighbor])
+                    {
+                        visited[neighbor] = true;
+#pragma omp critical
+                        {
+                            local_stack.push(neighbor);
+                        }
+                    }
                 }
             }
         }
-        front += level_size;
     }
-}
 
-// Parallel DFS function
-void parallel_dfs(int node, int num_nodes) {
-    visited[node] = true;
-    cout << "DFS Visited: " << node << endl;
+    
+    void parallel_bfs(int start)
+    {
+        visited[start] = true;
+        queue<int> q;
+        q.push(start);
 
-    #pragma omp parallel for
-    for (int i = 0; i < num_nodes; i++) {
-        if (adj[node][i] && !visited[i]) {
-            parallel_dfs(i, num_nodes);  // Recursive DFS
+#pragma omp parallel
+        {
+            queue<int> local_queue;
+            while (!q.empty())
+            {
+                int current;
+#pragma omp critical
+                {
+                    if (!q.empty())
+                    {
+                        current = q.front();
+                        q.pop();
+                    }
+                    else
+                    {
+                        current = -1;
+                    }
+                }
+
+                if (current == -1)
+                    continue;
+
+                for (int neighbor : graph[current])
+                {
+                    if (!visited[neighbor])
+                    {
+                        visited[neighbor] = true;
+#pragma omp critical
+                        {
+                            local_queue.push(neighbor);
+                        }
+                    }
+                }
+
+#pragma omp critical
+                {
+                    while (!local_queue.empty())
+                    {
+                        q.push(local_queue.front());
+                        local_queue.pop();
+                    }
+                }
+            }
         }
     }
-}
 
-int main() {
-    int num_nodes = 6;
+    void dfs(int start)
+    {
+        stack<int> s;
+        s.push(start);
+        visited[start] = true;
+        while (!s.empty())
+        {
+            int current = s.top();
+            s.pop();
+            for (int neighbor : graph[current])
+            {
+                if (!visited[neighbor])
+                {
+                    s.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+    }
 
-    // Example graph (adjacency matrix)
-    adj[0][1] = adj[1][0] = 1;
-    adj[0][2] = adj[2][0] = 1;
-    adj[1][3] = adj[3][1] = 1;
-    adj[1][4] = adj[4][1] = 1;
-    adj[2][5] = adj[5][2] = 1;
+    void bfs(int start)
+    {
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
+        while (!q.empty())
+        {
+            int current = q.front();
+            q.pop();
+            for (int neighbor : graph[current])
+            {
+                if (!visited[neighbor])
+                {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+    }
+};
 
-    // Parallel BFS
-    cout << "Parallel BFS:\n";
-    parallel_bfs(0, num_nodes);
+int main()
+{
+    int V = 1000000, E = 10000000;
+    Graph g(V, E);
+    double time_seq_dfs, time_par_dfs, time_seq_bfs, time_par_bfs;
 
-    // Reset visited array
-    fill(visited.begin(), visited.end(), false);
-
-    // Parallel DFS
-    cout << "\nParallel DFS:\n";
-    parallel_dfs(0, num_nodes);
+    g.initialize_visited();
+    auto start = chrono::high_resolution_clock::now();
+    g.dfs(0);
+    auto end = chrono::high_resolution_clock::now();
+    time_seq_dfs = chrono::duration<double>(end - start).count();
+    cout << "Time required for DFS sequential is:" << time_seq_dfs << endl;
+    g.initialize_visited();
+    start = chrono::high_resolution_clock::now();
+    g.parallel_dfs(0);
+    end = chrono::high_resolution_clock::now();
+    time_par_dfs = chrono::duration<double>(end - start).count();
+    cout << "Time required for DFS parallel is:" << time_par_dfs << endl;
+    g.initialize_visited();
+    start = chrono::high_resolution_clock::now();
+    g.bfs(0);
+    end = chrono::high_resolution_clock::now();
+    time_seq_bfs = chrono::duration<double>(end - start).count();
+    cout << "Time required for BFS sequential is:" << time_seq_bfs << endl;
+    g.initialize_visited();
+    start = chrono::high_resolution_clock::now();
+    g.parallel_bfs(0);
+    end = chrono::high_resolution_clock::now();
+    time_par_bfs = chrono::duration<double>(end - start).count();
+    cout << "Time required for BFS parallel is:" << time_par_bfs << endl;
+    cout << "DFS Speedup: " << (time_par_dfs > 0 ? time_seq_dfs / time_par_dfs : 0) << endl;
+    cout << "BFS Speedup: " << (time_par_bfs > 0 ? time_seq_bfs / time_par_bfs : 0) << endl;
 
     return 0;
 }
