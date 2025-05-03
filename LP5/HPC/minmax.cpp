59a2ffe1560faec2d47538083912c2f4dbd4cb7c
Vishwajeet Pratap Singh <101934336+vishwajeet0001@users.noreply.github.com>
2025-05-03 11:41:20 +0530

    Update minmax.cpp

--- LP5/HPC/minmax.cpp
+++ LP5/HPC/minmax.cpp
@@ -1,60 +1,163 @@
 #include <iostream>
-#include <vector>
 #include <omp.h>
+#include <vector>
+#include <climits>
+#include <cstdlib>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
-// Parallel Min Reduction
-int parallel_min(const vector<int>& arr) {
-    int min_value = arr[0];
-    #pragma omp parallel for reduction(min:min_value)
-    for (int i = 1; i < arr.size(); i++) {
-        min_value = min(min_value, arr[i]);
+void generateArray(vector<int> &arr, int n)
+{
+    for (int &num : arr)
+    {
+        num = rand() % 100;
     }
-    return min_value;
 }
 
-// Parallel Max Reduction
-int parallel_max(const vector<int>& arr) {
-    int max_value = arr[0];
-    #pragma omp parallel for reduction(max:max_value)
-    for (int i = 1; i < arr.size(); i++) {
-        max_value = max(max_value, arr[i]);
+double minSequential(const vector<int> &arr, int &min_val)
+{
+    auto start = high_resolution_clock::now();
+    min_val = INT_MAX;
+    for (int num : arr)
+    {
+        if (num < min_val)
+            min_val = num;
     }
-    return max_value;
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
 }
 
-// Parallel Sum Reduction
-int parallel_sum(const vector<int>& arr) {
-    int sum = 0;
-    #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < arr.size(); i++) {
-        sum += arr[i];
+double minParallel(const vector<int> &arr, int &min_val)
+{
+    auto start = high_resolution_clock::now();
+    min_val = INT_MAX;
+#pragma omp parallel for reduction(min : min_val)
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] < min_val)
+            min_val = arr[i];
     }
-    return sum;
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
 }
 
-// Parallel Average Calculation (using sum and size)
-double parallel_average(const vector<int>& arr) {
-    int sum = parallel_sum(arr);
-    return static_cast<double>(sum) / arr.size();
+double maxSequential(const vector<int> &arr, int &max_val)
+{
+    auto start = high_resolution_clock::now();
+    max_val = INT_MIN;
+    for (int num : arr)
+    {
+        if (num > max_val)
+            max_val = num;
+    }
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
 }
 
-int main() {
-    // Example vector
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+double maxParallel(const vector<int> &arr, int &max_val)
+{
+    auto start = high_resolution_clock::now();
+    max_val = INT_MIN;
+#pragma omp parallel for reduction(max : max_val)
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > max_val)
+            max_val = arr[i];
+    }
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
+}
 
-    // Parallel Min, Max, Sum, and Average
-    int min_value = parallel_min(arr);
-    int max_value = parallel_max(arr);
-    int sum_value = parallel_sum(arr);
-    double avg_value = parallel_average(arr);
+double sumSequential(const vector<int> &arr, long long &sum_val)
+{
+    auto start = high_resolution_clock::now();
+    sum_val = 0;
+    for (int num : arr)
+    {
+        sum_val += num;
+    }
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
+}
 
-    // Output results
-    cout << "Parallel Min: " << min_value << endl;
-    cout << "Parallel Max: " << max_value << endl;
-    cout << "Parallel Sum: " << sum_value << endl;
-    cout << "Parallel Average: " << avg_value << endl;
+double sumParallel(const vector<int> &arr, long long &sum_val)
+{
+    auto start = high_resolution_clock::now();
+    sum_val = 0;
+#pragma omp parallel for reduction(+ : sum_val)
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        sum_val += arr[i];
+    }
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
+}
+
+double avgSequential(const vector<int> &arr, double &avg_val)
+{
+    long long sum_val;
+    double time = sumSequential(arr, sum_val);
+    avg_val = static_cast<double>(sum_val) / arr.size();
+    return time;
+}
+
+double avgParallel(const vector<int> &arr, double &avg_val)
+{
+    long long sum_val;
+    double time = sumParallel(arr, sum_val);
+    avg_val = static_cast<double>(sum_val) / arr.size();
+    return time;
+}
+
+int main()
+{
+    omp_set_num_threads(4);
+
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    vector<int> arr(n);
+
+    generateArray(arr, n);
+
+    cout << "\nArray elements are: ";
+    for (int i = 0; i < min(n, 10); ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    int min_val, max_val;
+    long long sum_val;
+    double avg_val;
+
+    double t_min_seq = minSequential(arr, min_val);
+    cout << "\nSequential Min Value: " << min_val << "\nTime for Sequential Min: " << t_min_seq << " seconds";
+
+    double t_min_par = minParallel(arr, min_val);
+    cout << "\nParallel Min Value: " << min_val << "\nTime for Parallel Min: " << t_min_par << " seconds";
+    cout << "\nSpeedup for Min: " << (t_min_seq / t_min_par) << endl;
+
+    double t_max_seq = maxSequential(arr, max_val);
+    cout << "\nSequential Max Value: " << max_val << "\nTime for Sequential Max: " << t_max_seq << " seconds";
+
+    double t_max_par = maxParallel(arr, max_val);
+    cout << "\nParallel Max Value: " << max_val << "\nTime for Parallel Max: " << t_max_par << " seconds";
+    cout << "\nSpeedup for Max: " << (t_max_seq / t_max_par) << endl;
+
+    double t_sum_seq = sumSequential(arr, sum_val);
+    cout << "\nSequential Sum: " << sum_val << "\nTime for Sequential Sum: " << t_sum_seq << " seconds";
+
+    double t_sum_par = sumParallel(arr, sum_val);
+    cout << "\nParallel Sum: " << sum_val << "\nTime for Parallel Sum: " << t_sum_par << " seconds";
+    cout << "\nSpeedup for Sum: " << (t_sum_seq / t_sum_par) << endl;
+
+    double t_avg_seq = avgSequential(arr, avg_val);
+    cout << "\nSequential Average: " << avg_val << "\nTime for Sequential Average: " << t_avg_seq << " seconds";
+
+    double t_avg_par = avgParallel(arr, avg_val);
+    cout << "\nParallel Average: " << avg_val << "\nTime for Parallel Average: " << t_avg_par << " seconds";
+    cout << "\nSpeedup for Average: " << (t_avg_seq / t_avg_par) << endl;
 
     return 0;
 }
